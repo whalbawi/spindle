4a85cb2cc393adaf5ce2bd4b96b2caf276bb88cb
Wael Halbawi <waelhalbawi@gmail.com>
2022-04-21 08:32:55 -0700

    worker: Introduce deferred task execution
    Provide the ability to schedule a task for deferred execution. The
    scheduled task is stashed into a queue and the thread can continue
    processing enqueued tasks. To eliminate spinning, the worker thread goes
    to sleep once idle and waits on a condition variable with a timeout
    equal to the earliest scheduled task's deadline.
    
    Some tests included in this change check that scheduled work runs within
    a reasonable time. These tests are disabled in the macOS GitHub build
    job because the runners have poor and unpredictable performance.

--- src/worker.cpp
+++ src/worker.cpp
@@ -5,10 +5,34 @@ namespace spindle {
 void Worker::run() {
     for (;;) {
         std::function<void()> task;
-        std::unique_lock<std::mutex> lk(m);
+        std::unique_lock<std::mutex> lk{m};
 
-        cv.wait(lk, [this] { return terminated || !work.empty(); });
+        bool sched_task_due{};
+        // Wait until:
+        // - Terminated
+        // - There is a scheduled task due to run
+        // - There is a task to run
+        // - `cv` times out
+
+        // Note: `cv` will never time out with the predicate evaluating to false. This is because
+        //       The deadline is set if and only if there is scheduled work due.
+        cv.wait_until(lk, deadline, [this, &sched_task_due] {
+            clock::time_point now = clock::now();
+            sched_task_due = !sched_work.empty() && (now > sched_work.top().first);
+            return terminated || sched_task_due || !work.empty();
+        });
+
         if (terminated) return;
+
+        if (sched_task_due) {
+            // Enqueue the task for execution and reset `deadline` if there's scheduled work
+            // remaining
+            work.push(sched_work.top().second);
+            sched_work.pop();
+            deadline = sched_work.empty() ? clock::time_point::max() : sched_work.top().first;
+            sched_task_due = false;
+            continue;
+        }
 
         task = work.front();
         work.pop();
@@ -19,7 +43,7 @@ void Worker::run() {
 }
 
 bool Worker::enqueue(const std::function<void()>& task) {
-    std::lock_guard<std::mutex> lk(m);
+    std::lock_guard<std::mutex> lk{m};
     if (terminated) return false;
     work.emplace(task);
     cv.notify_one();
@@ -28,7 +52,7 @@ bool Worker::enqueue(const std::function<void()>& task) {
 }
 
 void Worker::terminate() {
-    std::lock_guard<std::mutex> lk(m);
+    std::lock_guard<std::mutex> lk{m};
     if (terminated) return;
     terminated = true;
     cv.notify_one();
