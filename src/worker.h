9ab599dc9ff1844bd867aaace712b8620c6e70c9
Wael Halbawi <waelhalbawi@gmail.com>
2022-04-22 09:23:32 -0700

    worker: Support scheduling periodic deferred tasks
    Provides timer-like functionality (no-cancellable though) enabling a
    task to run periodically.

--- src/worker.h
+++ src/worker.h
@@ -6,44 +6,64 @@
 #include <functional>
 #include <mutex>
 #include <queue>
-#include <utility>
 
 namespace spindle {
 
 using clock = std::chrono::high_resolution_clock;
 
-using sched_work_t = std::pair<clock::time_point, std::function<void()>>; // (deadline, task)
+class DeferredTask;
 
-// Sort in ascending order of execution time.
-static auto cmp = [](const sched_work_t& x, const sched_work_t& y) { return x.first > y.first; };
+using DeferredWorkCmp = std::function<bool(const DeferredTask& x, const DeferredTask& y)>;
 
 // `Worker` continuously executes tasks in a loop, until terminated.
 class Worker {
   public:
+    Worker();
     // Continuously executes enqueued tasks until terminated.
     void run();
     // Enqueues a task for execution.
     bool enqueue(const std::function<void()>& task);
     // Schedules a task for deferred execution.
-    template <class T> bool schedule(const std::function<void()>& task, T delay);
+    template <class T>
+    bool schedule(const std::function<void()>& task, T delay, bool periodic = false);
     // Terminates the `Worker`. From this point onwards, the `Worker` will not reject new tasks
     // but will continue executing any inflight task.
     void terminate();
 
   private:
     std::queue<std::function<void()>> work;
-    std::priority_queue<sched_work_t, std::vector<sched_work_t>, decltype(cmp)> sched_work{cmp};
-    std::mutex m;
-    std::condition_variable cv;
-    clock::time_point deadline = clock::time_point::max();
+    std::priority_queue<DeferredTask, std::vector<DeferredTask>, DeferredWorkCmp> deferred_work;
+    // Need a re-entrant mutex because `Worker::run` might call `Worker::schedule`.
+    // TODO (whalbawi): Can we refactor `Worker::schedule` to avoid this?
+    std::recursive_mutex m;
+    std::condition_variable_any cv;
+    clock::time_point deadline;
     bool terminated{};
 };
 
-template <class T> bool Worker::schedule(const std::function<void()>& task, T delay) {
-    std::lock_guard<std::mutex> lk{m};
+class DeferredTask {
+  public:
+    DeferredTask(std::function<void()> task,
+                 clock::time_point deadline,
+                 bool periodic,
+                 clock::duration delay);
+
+    friend Worker;
+
+  private:
+    static std::function<bool(const DeferredTask& x, const DeferredTask& y)> cmp;
+    std::function<void()> task;
+    clock::time_point deadline;
+    bool periodic;
+    clock::duration delay;
+};
+
+template <class T>
+bool Worker::schedule(const std::function<void()>& task, T delay, bool periodic) {
+    std::lock_guard<std::recursive_mutex> lk{m};
     if (terminated) return false;
     clock::time_point task_deadline = clock::now() + delay;
-    sched_work.emplace(task_deadline, task);
+    deferred_work.emplace(task, task_deadline, periodic, delay);
     deadline = std::min(deadline, task_deadline);
     cv.notify_one();
 
