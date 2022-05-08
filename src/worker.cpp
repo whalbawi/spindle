c7a7b78881258273c236342ee0670c06f956857f
Wael Halbawi <waelhalbawi@gmail.com>
2022-05-08 09:33:18 -0700

    worker: Eliminate recursive locking

--- src/worker.cpp
+++ src/worker.cpp
@@ -2,11 +2,11 @@
 
 namespace spindle {
 
-Worker::Worker() : work{Task::cmp}, deadline{clock::time_point::max()} {}
+Worker::Worker() : work{}, deadline{clock::time_point::max()} {}
 
 void Worker::run() {
     for (;;) {
-        std::unique_lock<std::recursive_mutex> lk{m};
+        std::unique_lock<std::mutex> lk{m};
 
         // Wait until:
         // - Terminated
@@ -43,19 +43,17 @@ void Worker::run() {
 }
 
 bool Worker::do_schedule(const Task& task) {
-    std::lock_guard<std::recursive_mutex> lk{m};
     if (terminated || draining) return false;
 
     work.push(task);
     deadline = std::min(deadline, work.top().deadline);
-    cv.notify_one();
 
     return true;
 }
 
 void Worker::drain() {
     {
-        std::lock_guard<std::recursive_mutex> lk{m};
+        std::lock_guard<std::mutex> lk{m};
         if (draining) return;
         draining = true;
         cv.notify_one();
@@ -64,19 +62,20 @@ void Worker::drain() {
 }
 
 void Worker::terminate() {
-    std::lock_guard<std::recursive_mutex> lk{m};
+    std::lock_guard<std::mutex> lk{m};
     if (terminated) return;
     terminated = true;
     cv.notify_one();
 }
-
-// Sort in ascending order of execution time.
-Task::Cmp Task::cmp = [](const Task& x, const Task& y) { return x.deadline > y.deadline; };
 
 Task::Task(std::function<void()> func,
            clock::duration delay,
            bool periodic,
            clock::time_point deadline)
     : func(std::move(func)), delay(delay), periodic(periodic), deadline(deadline) {}
+
+bool Task::operator>(const Task& other) const {
+    return deadline > other.deadline;
+}
 
 } // namespace spindle
